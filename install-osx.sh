d00546fb3cd45eaba1c8e1a99d64b543830528d6
Topher Redden <christopher.redden@gmail.com>
2016-04-09 22:31:57 +0100

    Updated vim RC

---
+++ install-osx.sh
@@ -0,0 +1,16 @@
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+files=".vimrc"
+folders="bundle colors"
+
+echo "Installing .vimrc..."
+for file in $files; do
+    ln -s $dir/$file ~/$file
+done
+
+echo "Installing .vim files..."
+mkdir -p ~/.vim
+for folder in $folders; do
+    gln -d $dir/.vim/$folder ~/.vim/$folder
+done
+
+echo "done."
