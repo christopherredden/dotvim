1f922591af12ba34445921050ad0f3af0fc3c7e7
Christopher Redden <git.sly830@passmail.net>
2025-05-04 23:19:45 +0200

    Updating installer

--- install.sh
+++ install.sh
@@ -2,13 +2,6 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Installing init.lua"
 mkdir -p ~/.config/nvim
-mkdir -p ~/.local/share/nvim/site/autoload
 ln -s $dir/init.lua ~/.config/nvim/init.lua
-
-#echo "Installing vim-plug..."
-#curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-#echo "Installing Plugins..."
-#nvim +PlugInstall +qall
 
 echo "done."
